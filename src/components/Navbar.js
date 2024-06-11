d14764f0da00e662ed470df2527c7ffe927b1a44
mike <mguerrero8816@gmail.com>
2024-06-11 16:16:27 -0700

    added navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,18 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top navbar-shrink">
-      <Link className="navbar-brand" to="/">Navbar</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contact</Link>
-          </li>
-        </ul>
+    <nav id="mainNav" className="navbar navbar-expand-lg navbar-light bg-dark fixed-top navbar-shrink">
+      <div className="container">
+        <a className="navbar-brand" href="#page-top">Mike Guerrero</a>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+            <li className="nav-item"><a className="nav-link" href="#services">Skills</a></li>
+            <li className="nav-item"><a className="nav-link" href="#portfolio">Projects</a></li>
+            <li className="nav-item"><a className="nav-link" href="#about">Timeline</a></li>
+            <li className="nav-item"><a className="nav-link" href="#team">About</a></li>
+            <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
